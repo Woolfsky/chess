e8afd2ca1580e0c259bc6d2a38f8cd545b7adf64
Skyler Woolf <skylerwoolf@gmail.com>
2024-03-15 10:39:50 -0600

    Implement a simple REPL in main

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -2,7 +2,8 @@ import chess.*;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        for (var i = 0; i < args.length; i++) {
+            System.out.printf("%d. %s%n", i+1, args[i]);
+        }
     }
 }

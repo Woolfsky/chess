e4f1cac6167373767cd0bb32b45dd2373392ef92
Skyler Woolf <skylerwoolf@gmail.com>
2024-03-15 10:55:06 -0600

    Start creating CommandHander class

---
+++ client/src/main/java/CommandHandler.java
@@ -0,0 +1,12 @@
+public class CommandHandler {
+    String[] p;
+    String s;
+    public CommandHandler(String[] parameters, String state) {
+        p = parameters;
+        s = state;
+    }
+
+    public String executeReturnState() {
+        return "";
+    }
+}
--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Chess! Type help to get started.");
+        String state = "Logged out";
         while (true) {
             System.out.printf("Type a command >>> ");
             Scanner scanner = new Scanner(System.in);
             String line = scanner.nextLine();
             var parameters = line.split(" ");
+
+            CommandHandler handler = new CommandHandler(parameters, state);
 
             for (var p : parameters) {
                 System.out.printf("You said: %s%n", p);

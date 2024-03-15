78bede00d36295074a2bc892fbbcda7351747988
Skyler Woolf <skylerwoolf@gmail.com>
2024-03-15 10:46:56 -0600

    Update REPL with continuous functionality

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,9 +1,18 @@
 import chess.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        for (var i = 0; i < args.length; i++) {
-            System.out.printf("%d. %s%n", i+1, args[i]);
+        System.out.println("Welcome to Chess! Type help to get started.");
+        while (true) {
+            System.out.printf("Type a command >>> ");
+            Scanner scanner = new Scanner(System.in);
+            String line = scanner.nextLine();
+            var parameters = line.split(" ");
+
+            for (var p : parameters) {
+                System.out.printf("You said: %s%n", p);
+            }
         }
     }
 }
